ae88f097b6a5f4df7f2211fe39e118153f0cfcd8
freddybugs <91549636+freddybugs@users.noreply.github.com>
2021-10-05 23:56:32 +0200

    ultime modification

--- exo08.rb
+++ exo08.rb
@@ -1,12 +1,12 @@
 puts "Bonjour, demarrage du compte a rebour..."
 puts "Choisi un chiffre entre 10 et 30 :"
 print "> "
-user_num = gets.chomp.to_i+1
+user_num = gets.chomp.to_i+1 # Choix du temps
 
-user_num.times do
-  user_num -= 1
+user_num.times do # Loop
+  user_num -= 1 # Decrementation du temps
   puts user_num
-  sleep 1
+  sleep 1 # Pause du systeme pendant 1 sec.
 end
 
-puts "BOOOOOOOOOOOM.....!!!!!  :))"
+puts "BOOOOOOOOOOOM.....!!!!!  :))" # Voila...
