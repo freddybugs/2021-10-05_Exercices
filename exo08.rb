c2528fe8ff8d1b62c390de23a9cb2d14e6578c70
freddybugs <91549636+freddybugs@users.noreply.github.com>
2021-10-05 23:53:59 +0200

    Dernier modif exercice du jour...

--- exo08.rb
+++ exo08.rb
@@ -1,11 +1,12 @@
 puts "Bonjour, demarrage du compte a rebour..."
 puts "Choisi un chiffre entre 10 et 30 :"
 print "> "
-user_num = gets.chomp.to_i
+user_num = gets.chomp.to_i+1
 
 user_num.times do
-  user_num -= 1 # Decrementation de la valeur
+  user_num -= 1
   puts user_num
   sleep 1
 end
-puts "BOOOOOOOM...!!! :))"
+
+puts "BOOOOOOOOOOOM.....!!!!!  :))"
